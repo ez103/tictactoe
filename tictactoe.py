efe50b9f6cccc3c2b5f7dfdec9bf886252730e19
Eric Zhang <ericzhang755@gmail.com>
2024-09-06 08:05:01 -0500

    update 8:05 am, friday september 6

--- tictactoe.py
+++ tictactoe.py
@@ -9,8 +9,23 @@ root.title("Tic-tac-toe")
 #frm.grid()
 #ttk.Label(frm, text="This is the TicTacToe game").grid(column=0, row=0)
 
+
+# X starts so true
+clicked = True
+count = 0
+
+# button clicked function
 def b_click(b):
-    pass
+    global clicked, count
+
+    if b["text"] == " " and clicked == True:
+        b["text"] = "X"
+        clicked = False
+        count += 1
+    elif b["text"] == " " and clicked == False:
+        b["text"] = "O"
+
+
 
 b1 = Button(root, text = " ", font = ("Garamond", 22), height = 3, width = 6, bg="SystemButtonFace", command = lambda: b_click(b1))
 b2 = Button(root, text = " ", font = ("Garamond", 22), height = 3, width = 6, bg="SystemButtonFace", command = lambda: b_click(b1))
@@ -26,16 +41,16 @@ b9 = Button(root, text = " ", font = ("Garamond", 22), height = 3, width = 6, bg
 
 
 b1.grid(row = 0, column = 0)
-b2.grid(row = 0, column = 0)
-b3.grid(row = 0, column = 0)
+b2.grid(row = 0, column = 1)
+b3.grid(row = 0, column = 2)
 
-b4.grid(row = 0, column = 0)
-b5.grid(row = 0, column = 0)
-b6.grid(row = 0, column = 0)
+b4.grid(row = 1, column = 0)
+b5.grid(row = 1, column = 1)
+b6.grid(row = 1, column = 2)
 
-b7.grid(row = 0, column = 0)
-b8.grid(row = 0, column = 0)
-b9.grid(row = 0, column = 0)
+b7.grid(row = 2, column = 0)
+b8.grid(row = 2, column = 1)
+b9.grid(row = 2, column = 2)
 
 
 
